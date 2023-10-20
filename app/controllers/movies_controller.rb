1c66fe1bfb4457c91c8e050852c9ec181c4ad5e8
Jacob Hoffmann <67609232+jacobhoffmann257@users.noreply.github.com>
2023-10-20 19:33:37 +0000

    params method added

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,56 +2,42 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
-
   def index
     @movies = Movie.order(created_at: :desc)
-
     respond_to do |format|
       format.json { render json: @movies }
-
       format.html
     end
   end
-
   def show
     @movie = Movie.find(params.fetch(:id))
   end
-
   def create
-    movie_params = params.require(:movie).permit(:title, :description)
-    
     @movie = Movie.new(movie_params)
-
     if @movie.valid?
       @movie.save
-
       redirect_to movies_url, notice: "Movie created successfully."
     else
       render "new"
     end
-  end
-
   def edit
     @movie = Movie.find(params.fetch(:id))
   end
-
   def update
-    @movie = Movie.find(params.fetch(:id))
-
-    movie_params = params.require(:movie).permit(:title, :description)
-    
+    @movie = Movie.find(params.fetch(:id))    
     if @movie.update(movie_params)
       redirect_to @movie, notice: "Movie updated successfully."
     else
       render "edit"
     end
   end
-
+end
+def movie_params
+  movie_params = params.require(:movie).permit(:title, :description)
+end
   def destroy
     @movie = Movie.find(params.fetch(:id))
-
     @movie.destroy
-
     redirect_to movies_url, notice: "Movie deleted successfully."
   end
 end
